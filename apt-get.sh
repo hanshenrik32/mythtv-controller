4eeee3796d3c6cfbf28ed9585930a7a78c14be8a
Hans-Henrik Grüner-Pedersen <hans@linuxboxen.dk>
2017-08-27 23:11:46 +0200

    update

---
+++ apt-get.sh
@@ -0,0 +1,1 @@
+sudo apt-get install g++ make libcurl-ocaml-dev libdevil-dev libsdl1.2-dev glutg3-dev libmysqlclient-dev libsdl-image1.2-dev lirc-modules-source dialog imagemagick liblircclient-dev xmltv libglc-dev libical-dev unzip libical-dev unzip libical-dev lirc libsqlite3-dev libvlc-dev libxrandr-dev libxml2-dev
