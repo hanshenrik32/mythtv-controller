5adb6131da60a7c1c5ed1c9f955108cb4dfea7de
Hans-Henrik Grüner-Pedersen <hans@linuxboxen.dk>
2018-08-17 11:30:14 +0200

    add debian build stuf

---
+++ debian/mythtv-controller.default.ex
@@ -0,0 +1,10 @@
+# Defaults for mythtv-controller initscript
+# sourced by /etc/init.d/mythtv-controller
+# installed at /etc/default/mythtv-controller by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
