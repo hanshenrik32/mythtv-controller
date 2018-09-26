db45fa88f465cb6e0d7ea1122185c9b878c50049
Hans-Henrik Grüner-Pedersen <hans@linuxboxen.dk>
2018-09-26 15:19:54 +0200

    rss update
    Et langsomt mord

--- myctrl_stream.cpp
+++ myctrl_stream.cpp
@@ -1779,6 +1779,18 @@ if (check_rss_feed_exist(conn,"Manden, der blev stjålet")==0) {
       rss_update=true;
     }
 
+    //  Et langsomt mord
+    if (check_rss_feed_exist(conn,"Et langsomt mord")==0) {
+      sprintf(sqlselect,"REPLACE INTO mythtvcontroller.internetcontent(name,thumbnail,type,author,description,commandline,version,updated,search,tree,podcast,download,host) VALUES ('Et langsomt mord',NULL,0,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL)");
+      if (mysql_query(conn,sqlselect)!=0) printf("mysql insert error Tilbage til Mars.");
+      res = mysql_store_result(conn);
+      mysql_free_result(res);
+      sprintf(sqlselect,"REPLACE INTO mythtvcontroller.internetcontentarticles (feedtitle,path,paththumb,title,season,episode,description,url,type,thumbnail,mediaURL,author,date,time,rating,filesize,player,playerargs,download,downloadargs,width,height,language,podcast,downloadable,customhtml,countries) VALUES ('Et langsomt mord',NULL,NULL,'Et langsomt mord',0,0,NULL,'https://www.dr.dk/mu/feed/et-stille-mord.xml?format=podcast&limit=500',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL)");
+      if (mysql_query(conn,sqlselect)!=0) printf("mysql insert error Tilbage til Mars.\n");
+      res = mysql_store_result(conn);
+      mysql_free_result(res);
+      rss_update=true;
+    }
 
 
 
