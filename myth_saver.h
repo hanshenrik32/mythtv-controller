91b709aec953d9573a04eac34814a65ceb49084f
Hans-Henrik Grüner-Pedersen <hanshenrik32@gmail.com>
2018-11-04 23:05:45 +0100

    update

--- myth_saver.h
+++ myth_saver.h
@@ -31,7 +31,7 @@ class boxarray {
     void set_new_textures(int mode,int type,GLuint newtexture);
     void settexture(struct music_oversigt_type *musicoversigt);
     void show_music_3d(int aangle,GLuint textureId,GLuint textureId2,GLuint textureId3);
-    void show_music_3d_new(int aangle,GLuint textureId);
+    //void show_music_3d_new(int aangle,GLuint textureId);
     void show_music_3d1(float aangle,GLuint textureId);
     void loadboxpictures();
     void createboxpictures();
