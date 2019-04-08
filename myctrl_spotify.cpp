fa7b44cb6d6ccbce9b4a1b5e745e6a85da5c347c
Hans-Henrik Grüner-Pedersen <hanshenrik32@gmail.com>
2019-04-08 22:52:13 +0200

    json parser update

--- myctrl_spotify.cpp
+++ myctrl_spotify.cpp
@@ -13,6 +13,8 @@
 #include <curl/curl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include "json-parser/json.h"
+
 //#include <http_parser.h>
 
 // web server stuf
@@ -228,19 +230,125 @@ int spotify_class::spotify_get_users_playlist() {
     sprintf(doget,"curl -X 'GET' 'https://api.spotify.com/v1/me/playlists' -H 'Content-Type: application/json' -H 'Authorization: Bearer %s' > spotify_users_playlist.txt",spotify_authorize_token);
     system(doget);
   }
+
 }
 
 
 
-// get user play list
+
+/* ***************************************************************************************** */
+
+
+
+static void print_depth_shift(int depth)
+{
+  int j;
+  for (j=0; j < depth; j++) {
+    printf(" ");
+  }
+}
+
+static void process_value(json_value* value, int depth);
+
+static void process_object(json_value* value, int depth) {
+  int length, x;
+  if (value == NULL) {
+    return;
+  }
+  length = value->u.object.length;
+  for (x = 0; x < length; x++) {
+    print_depth_shift(depth);
+    printf("object[%d].name = %s\n", x, value->u.object.values[x].name);
+    process_value(value->u.object.values[x].value, depth+1);
+  }
+}
+
+static void process_array(json_value* value, int depth)
+{
+  int length, x;
+  if (value == NULL) {
+    return;
+  }
+  length = value->u.array.length;
+  printf("array\n");
+  for (x = 0; x < length; x++) {
+    process_value(value->u.array.values[x], depth);
+  }
+}
+
+static void process_value(json_value* value, int depth)
+{
+int j;
+if (value == NULL) {
+  return;
+}
+if (value->type != json_object) {
+  print_depth_shift(depth);
+}
+switch (value->type) {
+  case json_none:
+    printf("none\n");
+    break;
+  case json_object:
+    process_object(value, depth+1);
+    break;
+  case json_array:
+    process_array(value, depth+1);
+    break;
+  case json_integer:
+    printf("int: %10" PRId64 "\n", value->u.integer);
+    break;
+  case json_double:
+    printf("double: %f\n", value->u.dbl);
+    break;
+  case json_string:
+    printf("string: %s\n", value->u.string.ptr);
+    break;
+  case json_boolean:
+    printf("bool: %d\n", value->u.boolean);
+    break;
+  }
+}
+
+
+// get user play list (any public user)
 // write to spotify_playlist.txt
 
 int spotify_class::spotify_get_playlist(char *playlist) {
   char doget[4096];
+  FILE *json_file;
+  char *jons_string;
+  struct stat filestatus;
+  int file_size;
+  char* file_contents;
+  json_char* json;
+  json_value* value;
   if (strcmp(spotify_authorize_token,"")!=0) {
     sprintf(doget,"curl -X 'GET' 'https://api.spotify.com/v1/playlists/%s' -H 'Content-Type: application/json' -H 'Authorization: Bearer %s' > spotify_playlist.txt",playlist,spotify_authorize_token);
     system(doget);
   }
+  stat("spotify_playlist.txt", &filestatus);
+  file_size=filestatus.st_size;                        // get filesize
+  file_contents = (char*)malloc(filestatus.st_size);
+  json_file = fopen("spotify_playlist.txt", "rt");
+  if ( fread(file_contents, file_size, 1, json_file ) != 1 ) {
+    fprintf(stderr, "Unable t read content of %s\n", "spotify_playlist.txt");
+    fclose(json_file);
+    free(file_contents);
+    return 1;
+  }
+  fclose(json_file);
+  json = (json_char*)file_contents;
+  value = json_parse(json,file_size);
+   //spotify_playlist.txt
+
+
+  process_value(value, 0);
+
+
+  json_value_free(value);
+  free(file_contents);
+  exit(0);
 }
 
 
