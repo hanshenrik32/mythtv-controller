1d1a9abe6eded57789402a2f8ce6a89c7e359186
Hans-Henrik Grüner-Pedersen <hanshenrik32@gmail.com>
2019-07-04 22:51:31 +0200

    now use libcurl to play and status on spotify player

--- myctrl_spotify.cpp
+++ myctrl_spotify.cpp
@@ -1288,7 +1288,8 @@ size_t curl_writeFunction(void *ptr, size_t size, size_t nmemb, std::string* dat
     return size * nmemb;
 }
 
-//std::string *response_string;
+// in use
+// ********************************************************************************************
 
 int spotify_class::spotify_do_we_play2() {
   char auth_kode[1024];
@@ -1328,6 +1329,8 @@ int spotify_class::spotify_do_we_play2() {
   }
 }
 
+
+// NOT in use
 
 //
 // work
@@ -1513,8 +1516,8 @@ int spotify_class::spotify_next_play() {
 }
 
 
-//
-// next play next song
+// DO not work for now
+// next play next song need testing
 //
 
 int spotify_class::spotify_next_play2() {
@@ -1522,8 +1525,6 @@ int spotify_class::spotify_next_play2() {
   std::string response_string;
   int httpCode;
   CURLcode res;
-  json_char *json;
-  json_value *value;
   struct curl_slist *chunk = NULL;
   strcpy(auth_kode,"Authorization: Bearer ");
   strcat(auth_kode,spotifytoken);
@@ -1538,6 +1539,8 @@ int spotify_class::spotify_next_play2() {
     chunk = curl_slist_append(chunk, "Content-Type: application/json");
     chunk = curl_slist_append(chunk, auth_kode);
     curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
+    /* Now specify we want to POST data */
+    curl_easy_setopt(curl, CURLOPT_POST, 1L);
     res = curl_easy_perform(curl);
     curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &httpCode);
     if (res != CURLE_OK) {
@@ -1562,19 +1565,145 @@ int spotify_class::spotify_next_play2() {
 // 404	Not Found - The requested resource could not be found. This error can be due to a temporary or permanent condition
 // 429	Too Many Requests - Rate limiting has been applied.
 
-int spotify_class::spotify_play_now_playlist(char *playlist_song,bool now) {
+int spotify_class::spotify_play_now_playlist2(char *playlist_song,bool now) {
   int curl_error;
   char call[4096];
   char *devid=spotify_oversigt.get_active_device_id();
   devid[40]='\0';
-  printf("Devid *%s*\n",devid);
   sprintf(call,"curl -f -X PUT 'https://api.spotify.com/v1/me/player/play?device_id=%s' --data \"{\\\"context_uri\\\":\\\"spotify:playlist:%s\\\",\\\"offset\\\":{\\\"position\\\":5},\\\"position_ms\\\":0}\" -H \"Content-Type: application/json\" -H 'Authorization: Bearer %s'",devid,playlist_song,spotifytoken);
+  printf("Devid *%s*\n",devid);
   curl_error=system(call);
   if (WEXITSTATUS(curl_error)!=0) {
     fprintf(stderr,"Error start play %d \n",WEXITSTATUS(curl_error));
   }
   return(curl_error);
 }
+
+
+// ************** DEBUG CODE **********************
+
+
+int debug_callback(CURL *handle,curl_infotype type,char *data,size_t size,void *userptr);
+
+//CURLcode curl_easy_setopt(CURL *handle, CURLOPT_DEBUGFUNCTION,debug_callback);
+
+static void dump(const char *text,FILE *stream, unsigned char *ptr, size_t size) {
+  size_t i;
+  size_t c;
+  unsigned int width=0x10;
+
+  fprintf(stream, "%s, %10.10ld bytes (0x%8.8lx)\n",
+          text, (long)size, (long)size);
+
+  for(i=0; i<size; i+= width) {
+    fprintf(stream, "%4.4lx: ", (long)i);
+
+    /* show hex to the left */
+    for(c = 0; c < width; c++) {
+      if(i+c < size)
+        fprintf(stream, "%02x ", ptr[i+c]);
+      else
+        fputs("   ", stream);
+    }
+
+    /* show data on the right */
+    for(c = 0; (c < width) && (i+c < size); c++) {
+      char x = (ptr[i+c] >= 0x20 && ptr[i+c] < 0x80) ? ptr[i+c] : '.';
+      fputc(x, stream);
+    }
+
+    fputc('\n', stream); /* newline */
+  }
+}
+
+static int my_trace(CURL *handle, curl_infotype type, char *data, size_t size, void *userp) {
+  const char *text;
+  (void)handle; /* prevent compiler warning */
+  (void)userp;
+
+  switch (type) {
+  case CURLINFO_TEXT:
+    fprintf(stderr, "== Info: %s", data);
+  default: /* in case a new one is introduced to shock us */
+    return 0;
+
+  case CURLINFO_HEADER_OUT:
+    text = "=> Send header";
+    break;
+  case CURLINFO_DATA_OUT:
+    text = "=> Send data";
+    break;
+  case CURLINFO_SSL_DATA_OUT:
+    text = "=> Send SSL data";
+    break;
+  case CURLINFO_HEADER_IN:
+    text = "<= Recv header";
+    break;
+  case CURLINFO_DATA_IN:
+    text = "<= Recv data";
+    break;
+  case CURLINFO_SSL_DATA_IN:
+    text = "<= Recv SSL data";
+    break;
+  }
+  dump(text, stderr, (unsigned char *)data, size);
+  return 0;
+}
+
+// **********************************************************************************
+
+
+int spotify_class::spotify_play_now_playlist(char *playlist_song,bool now) {
+  std::string auth_kode;
+  std::string response_string;
+  std::string url;
+  char post_playlist_data[4096];
+  int httpCode;
+  CURLcode res;
+  struct curl_slist *chunk = NULL;
+  char *devid=spotify_oversigt.get_active_device_id();
+  auth_kode="Authorization: Bearer ";
+  auth_kode=auth_kode + spotifytoken;
+  url="https://api.spotify.com/v1/me/player/play?device_id=";
+  url=url + devid;
+  curl_global_init(CURL_GLOBAL_ALL);
+  CURL *curl = curl_easy_init();
+  if (curl) {
+    /* Add a custom header */
+    chunk = curl_slist_append(chunk, "Accept: application/json");
+    chunk = curl_slist_append(chunk, "Content-Type: application/json");
+    chunk = curl_slist_append(chunk, "charsets: utf-8");
+    chunk = curl_slist_append(chunk, auth_kode.c_str());
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curl_writeFunction);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (char *) &response_string);
+    //curl_easy_setopt(curl, CURLOPT_DEBUGFUNCTION, my_trace);
+    curl_easy_setopt(curl, CURLOPT_VERBOSE, 0L);                                    // enable stdio echo
+    curl_easy_setopt(curl, CURLOPT_HEADER, 1L);
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
+    curl_easy_setopt(curl, CURLOPT_POST, 1);
+    // set type post/put
+    curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "PUT"); /* !!! */
+    sprintf(post_playlist_data,"{\"context_uri\":\"spotify:playlist:%s\",\"offset\":{\"position\":5},\"position_ms\":0}",playlist_song);
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post_playlist_data );
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, strlen(post_playlist_data));
+    res = curl_easy_perform(curl);
+    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &httpCode);
+    if (res != CURLE_OK) {
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",curl_easy_strerror(res));
+    }
+    // always cleanup
+    curl_easy_cleanup(curl);
+    curl_global_cleanup();
+    if (httpCode == 200) {
+      return(1);
+    }
+  }
+  return(0);
+}
+
+
+
 
 
 
@@ -1612,6 +1741,8 @@ int spotify_class::spotify_play_now_song(char *playlist_song,bool now) {
   }
   return(curl_error);
 }
+
+
 
 // work
 // play artist
