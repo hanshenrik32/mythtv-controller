78c533a2b1fefcdae2537527aef3c970239a7516
Hans-Henrik Grüner-Pedersen <hans@linuxboxen.dk>
2020-02-03 10:20:34 +0100

    more rss

--- myctrl_stream.cpp
+++ myctrl_stream.cpp
@@ -3202,6 +3202,25 @@ int stream_class::opdatere_stream_oversigt(char *art,char *fpath) {
 
 
 
+      // Tech fra toppen
+      if (check_rss_feed_exist(conn,(char *) "Tech fra toppen")==0) {
+        sprintf(sqlselect,"REPLACE INTO mythtvcontroller.internetcontent(name,thumbnail,type,author,description,commandline,version,updated,search,tree,podcast,download,host) VALUES ('Tech fra toppen',NULL,0,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL)");
+        res = mysql_store_result(conn);
+        mysql_free_result(res);
+        if (mysql_query(conn,sqlselect)!=0) printf("mysql insert error Tech fra toppen\n");
+        sprintf(sqlselect,"REPLACE INTO mythtvcontroller.internetcontentarticles (feedtitle,path,paththumb,title,season,episode,description,url,type,thumbnail,mediaURL,author,date,time,rating,filesize,player,playerargs,download,downloadargs,width,height,language,podcast,downloadable,customhtml,countries) VALUES ('Tech fra toppen','Tech fra toppen',0,0,NULL,'http://www.spreaker.com/show/2832975/episodes/feed',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL)");
+        res = mysql_store_result(conn);
+        mysql_free_result(res);
+        if (mysql_query(conn,sqlselect)!=0) {
+          printf("mysql insert error Tech fra toppen.\n");
+          printf("SQL: %s\n",sqlselect);
+        }
+        rss_update=true;
+      }
+
+
+
+
       // close mysql
       if (conn) mysql_close(conn);
       // download new rrs files we just insert in db
