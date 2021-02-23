e7a38f26a6f09ee4136d3aa7293942fb2b83d765
Hans-Henrik Grüner-Pedersen <hanshenrik32@gmail.com>
2021-02-23 18:43:24 +0100

    fmod update

---
+++ apt-get_20.04.sh
@@ -0,0 +1,2 @@
+sudo apt-get install liblirc-dev libical-dev libxml++2.6-dev libvlc-dev libcurl4-openssl-dev libsqlite3-dev libsdl2-dev libsdl-image1.2-dev libfreetype-dev freeglut3-dev mysql-server libglc-dev libmysqlclient-dev g++ make libdevil-dev libcurl-ocaml-dev libsdl1.2-dev freeglut3-dev libsdl-image1.2-dev dialog imagemagick liblircclient-dev xmltv libglc-dev libical-dev unzip libical-dev unzip libical-dev lirc libsdl-mixer1.2-dev libsqlite3-dev libvlc-dev libxrandr-dev libxml2-dev libhttp-parser-dev
+
