3dc583e45d2c564c1aaad9b207fcc97169e13985
hans <hanshenrik32@gmail.com>
2023-08-27 21:10:00 +0200

    update clean output

--- main.cpp
+++ main.cpp
@@ -3649,8 +3649,7 @@ void display() {
       }
     }
     
-    printf("snd=%d playstatus %d  do_zoom_music_cover=%d  ask_open_dir_or_play=%d  \n",snd, musicoversigt.play_songs_status() , do_zoom_music_cover, ask_open_dir_or_play );
-    
+       
     
     // show volume value
     if (show_volume_info) {
