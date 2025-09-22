902e33c8296238292bc5f270653daa30c2abc1aa
Hans-Henrik Grüner-Pedersen <hanshenrik32@gmail.com>
2025-09-22 22:42:48 +0200

    update

--- apt-get.sh
+++ apt-get.sh
@@ -1,3 +1,3 @@
-sudo apt-get install g++ make curl libcurl-ocaml-dev libdevil-dev libsdl1.2-dev freeglut3-dev libsdl-image1.2-dev dialog imagemagick xmltv libglc-dev libical-dev unzip libical-dev unzip libical-dev lirc libsqlite3-dev libvlc-dev libxrandr-dev libxml2-dev libhttp-ocaml-dev libmysqlclient-dev libmediainfo-dev libfreetype-dev libfreetype6 libfreetype6-dev libfmt-dev  libglfw3 libglfw3-dev libjsoncpp-dev libtorrent-rasterbar-dev libtorrent-dev libfmt-dev liblircclient-dev
+sudo apt-get install g++ make curl libcurl-ocaml-dev libdevil-dev libsdl1.2-dev freeglut3-dev libsdl-image1.2-dev dialog imagemagick xmltv libglc-dev libical-dev unzip libical-dev unzip libical-dev lirc libsqlite3-dev libvlc-dev libxrandr-dev libxml2-dev libhttp-ocaml-dev libmysqlclient-dev libmediainfo-dev libfreetype-dev libfreetype6 libfreetype6-dev libfmt-dev  libglfw3 libglfw3-dev libjsoncpp-dev libtorrent-rasterbar-dev libtorrent-dev libfmt-dev liblircclient-dev libmediainfo-dev
 
 
